1ad8ab0bd2d57e9de2c34ba012661baecbfa1726
Ratnesh Kumar <ratneshk.99@gmail.com>
2024-11-23 21:31:25 -0600

    Link config storage to api

--- app/api/query_handler.py
+++ app/api/query_handler.py
@@ -1,6 +1,8 @@
 from app.services.openai import OpenAIClient
 from app.services.kubernetes.workload import workload_handler
 from app.services.kubernetes.services import service_handler
+from app.services.kubernetes.config_storage import config_storage_handler
+
 
 def handle_query(query: str, openai_client: OpenAIClient = OpenAIClient())->str:
 
@@ -12,6 +14,9 @@ def handle_query(query: str, openai_client: OpenAIClient = OpenAIClient())->str:
         return workload_handler(query_details)
     elif query_details.resource_category =="services":
         return service_handler(query_details)
+    elif query_details.resource_category == "config_storage":
+        return config_storage_handler(query_details)
+
 
     response = "Dummy"
 
--- app/services/kubernetes/config_storage/__init__.py
+++ app/services/kubernetes/config_storage/__init__.py
@@ -4,7 +4,7 @@ from .secrets import secrets_handler
 from .storage_class import storage_class_handler
 
 
-def service_handler(query):
+def config_storage_handler(query):
 
     if query.resource_type == "config_maps":
         return config_maps_hamdler(query)

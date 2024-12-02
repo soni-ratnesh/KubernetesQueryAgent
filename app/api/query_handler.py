eb78551a41e8f90c9ef0cfcabec76fb0fda3b1d4
Ratnesh Kumar <ratneshk.99@gmail.com>
2024-12-02 03:47:04 -0600

    Register cluster handler in app

--- app/api/query_handler.py
+++ app/api/query_handler.py
@@ -2,22 +2,22 @@ from app.services.openai import OpenAIClient
 from app.services.kubernetes.workload import workload_handler
 from app.services.kubernetes.services import service_handler
 from app.services.kubernetes.config_storage import config_storage_handler
-
+from app.services.kubernetes.cluster import cluster_handler
 
 def handle_query(query: str, openai_client: OpenAIClient = OpenAIClient())->str:
 
     # extract query key information using OpenAI
     query_details = openai_client.extract(query)
 
-    # query kube client
+    # query kube client based on resourse category
     if query_details.resource_category=="workload":
         return workload_handler(query_details)
     elif query_details.resource_category =="services":
         return service_handler(query_details)
     elif query_details.resource_category == "config_storage":
         return config_storage_handler(query_details)
-
-
-    response = "Dummy"
-
-    return response
+    elif query_details.resource_category == "cluster":
+        return cluster_handler(query_details)
+    else:
+        return "Unknown Resource Category"
+    
